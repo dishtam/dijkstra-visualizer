cd9e98340104b40f50863d63f79f2143401b2a4f
Rohit Roy <rohhiiiit@gmail.com>
2023-06-25 00:57:21 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/dijkstra-visualizer">
+      <Routes>
+        <Route path="/" element={<PathfindingVisualizer />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
